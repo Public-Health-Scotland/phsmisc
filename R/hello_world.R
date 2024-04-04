25ae484d722a8a94be74a72e217116d1d4e7fa8d
James McMahon <james.mcmahon@phs.scot>
2024-04-04 14:11:55 +0100

    Document hello_world

--- R/hello_world.R
+++ R/hello_world.R
@@ -1,3 +1,12 @@
+#' Hello World
+#'
+#' @param name Your name
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' hello_world("James")
 hello_world <- function(name) {
 
   response <- paste0("Hello ", name, "!")
