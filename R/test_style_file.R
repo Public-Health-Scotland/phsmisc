1e29ccd39d595e631f2ab619a9ec52407970db59
James McMahon <james.mcmahon@phs.scot>
2024-10-23 12:32:49 +0100

    Update test_style_file.R

--- R/test_style_file.R
+++ R/test_style_file.R
@@ -1,11 +1,12 @@
 # Test file to check the style action works
 
-obj <- "name"
+obj = "name"
 
 #' Not a real function
 #'
 #' @description
 #' Just a test function - used to test the GH actions only
-fun <- function(var = 1) {
+#' @param var any value
+fun <- function(var =1) {
   return(var)
 }
