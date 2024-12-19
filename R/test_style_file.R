2bf8db2ef4aca352622b8b05fff52f8d3ce8932c
Moohan <Moohan@users.noreply.github.com>
2024-12-19 10:55:13 +0000

    Style code (GHA)

--- R/test_style_file.R
+++ R/test_style_file.R
@@ -1,12 +1,12 @@
 # Test file to check the style action works
 
-obj = "name"
+obj <- "name"
 
 #' Not a real function
 #'
 #' @description
 #' Just a test function - used to test the GH actions only
 #' @param var any value
-fun <- function(var =1) {
+fun <- function(var = 1) {
   return(var)
 }
