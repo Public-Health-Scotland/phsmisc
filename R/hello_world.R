58a0cc42ad1b1ef7aecd3a118cd8fefd4e42845c
James McMahon <james.mcmahon@phs.scot>
2025-03-20 10:26:02 +0000

    Update hello_world.R

--- R/hello_world.R
+++ R/hello_world.R
@@ -1,6 +1,6 @@
 #' Hello World
 #'
-#' Some description text
+#' Some description texty
 #'
 #' @param name Your name
 #'
@@ -12,7 +12,7 @@
 hello_world <- function(name) {
   stopifnot(inherits(name, "character"))
 
-  response <- paste0("Hello ", name, "!")
+  response <- paste0("Hello " , name, "!")
 
   return(response)
 }
