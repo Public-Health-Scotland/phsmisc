ca390c6abbea021729d86763e0ca68e4671e3a08
Moohan <5982260+Moohan@users.noreply.github.com>
2025-03-20 10:38:06 +0000

    Style code (GHA)

--- R/hello_world.R
+++ R/hello_world.R
@@ -10,7 +10,7 @@
 #' @examples
 #' hello_world("James")
 hello_world <- function(name) {
-  stopifnot(inherits(name , "character"))
+  stopifnot(inherits(name, "character"))
 
   response <- paste0("Hello ", name, "!")
 
